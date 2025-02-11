8d817add20cd8676b08e8c9f7de9a3796595363a
Ruslan <noname0263@gmail.com>
2025-02-12 02:00:31 +0900

    add url shortener service

---
+++ internal/service/service.go
@@ -0,0 +1,62 @@
+package service
+
+import (
+	"errors"
+	"github.com/lifedaemon-kill/ozon-url-shortener-api/internal/pkg/config"
+	"github.com/lifedaemon-kill/ozon-url-shortener-api/internal/pkg/internal_errors"
+	"github.com/lifedaemon-kill/ozon-url-shortener-api/internal/pkg/lib"
+	"github.com/lifedaemon-kill/ozon-url-shortener-api/internal/storage"
+	"log/slog"
+)
+
+type UrlShortener interface {
+	CreateAlias(sourceURL string) (aliasURL string, err error)
+	FetchSource(aliasURL string) (sourceURL string, err error)
+}
+
+type service struct {
+	repo      storage.Storage
+	log       *slog.Logger
+	genConfig config.URLGenerator
+}
+
+func New(conf config.URLGenerator, repo storage.Storage, log *slog.Logger) UrlShortener {
+	return &service{
+		repo:      repo,
+		log:       log,
+		genConfig: conf,
+	}
+}
+
+func (s *service) CreateAlias(sourceURL string) (string, error) {
+	//Генерируем новую ссылку
+	alias := lib.GenerateLinkStrBuilder(s.genConfig.URLLength, s.genConfig.AllowedSymbols)
+
+	//Проверяем, есть ли такая уже в бд
+	_, err := s.repo.FetchURL(alias)
+
+	//В случае если произошла коллизия, нужно перегенерировать ссылку
+	for err == nil {
+		alias = lib.GenerateLinkStrBuilder(s.genConfig.URLLength, s.genConfig.AllowedSymbols)
+		_, err = s.repo.FetchURL(alias)
+	}
+
+	//Проверяем по ошибке, что в бд нет такой строки, и добавляем
+	if errors.Is(err, ierrors.NoSuchValue) {
+		if err = s.repo.SaveURL(sourceURL, alias); err != nil {
+			return "", err
+		}
+		return alias, nil
+	}
+	//Иначе что-то отвалилось в бд...
+	return "", err
+
+}
+
+func (s *service) FetchSource(aliasURL string) (sourceURL string, err error) {
+	source, err := s.repo.FetchURL(aliasURL)
+	if err != nil {
+		return "", err
+	}
+	return source, nil
+}
